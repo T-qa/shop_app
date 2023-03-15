fff1c179eeef5a77b604e21ebac0691a64f5da41
Tit-qa <quanganhtrinh1804@gmail.com>
2023-03-15 15:42:41 +0700

    add product detail screen and navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/product_detail_screen.dart';
 import './screens/product_overview_screen.dart';
 
 void main() {
@@ -14,10 +15,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Shop',
       theme: ThemeData(
-          fontFamily: 'Lato',
+          //fontFamily: 'Lato',
           colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
               .copyWith(secondary: Colors.deepOrange)),
       home: ProductOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName: (context) => ProductDetailScreen()
+      },
     );
   }
 }
