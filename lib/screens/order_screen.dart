da9f7ce80d86e5906d0de1be1e1041af935e1ce2
Tit-qa <quanganhtrinh1804@gmail.com>
2023-03-20 23:22:32 +0700

    using future builder in order screen

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -4,7 +4,6 @@ import '../widgets/order_product.dart';
 import '../provider/order_provider.dart';
 import '../widgets/drawer.dart';
 
-
 class OrdersScreen extends StatefulWidget {
   static const routeName = '/order';
 
@@ -13,18 +12,47 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
+  Future? _orderFuture;
+  Future _obtainOrderFuture() {
+    return Provider.of<OrderProvider>(context, listen: false)
+        .fetchAndSetOrders();
+  }
+
+  @override
+  void initState() {
+    _orderFuture = _obtainOrderFuture();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final orderProvider = Provider.of<OrderProvider>(context);
     return Scaffold(
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
       appBar: AppBar(
         title: const Text('Your Orders'),
       ),
-      body: ListView.builder(
-        itemCount: orderProvider.orders.length,
-        itemBuilder: (context, index) =>
-            OrderProduct(orderProvider.orders[index]),
+      body: FutureBuilder(
+        future: _orderFuture,
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              // ...
+              // Do error handling stuff
+              return const Center(
+                child: Text('An error occurred!'),
+              );
+            } else {
+              return Consumer<OrderProvider>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) => OrderProduct(orderData.orders[i]),
+                ),
+              );
+            }
+          }
+        },
       ),
     );
   }
